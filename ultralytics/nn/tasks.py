e52f8797583ce4d87d6e4523209891ff199cf1d2
SAI <taipingeric@gmail.com>
2023-03-21 19:11:08 +0800

    Update tasks.py
    update GAM

--- ultralytics/nn/tasks.py
+++ ultralytics/nn/tasks.py
@@ -458,7 +458,8 @@ def parse_model(d, ch, verbose=True):  # model_dict, input_channels(3)
 
         n = n_ = max(round(n * depth), 1) if n > 1 else n  # depth gain
         if m in (Classify, Conv, ConvTranspose, GhostConv, Bottleneck, GhostBottleneck, SPP, SPPF, DWConv, Focus,
-                 BottleneckCSP, C1, C2, C2f, C3, C3TR, C3Ghost, nn.ConvTranspose2d, DWConvTranspose2d, C3x):
+                 BottleneckCSP, C1, C2, C2f, C3, C3TR, C3Ghost, nn.ConvTranspose2d, DWConvTranspose2d, C3x,
+                 GAM):
             c1, c2 = ch[f], args[0]
             if c2 != nc:  # if c2 not equal to number of classes (i.e. for Classify() output)
                 c2 = make_divisible(min(c2, max_channels) * width, 8)
@@ -475,11 +476,11 @@ def parse_model(d, ch, verbose=True):  # model_dict, input_channels(3)
             args.append([ch[x] for x in f])
             if m is Segment:
                 args[2] = make_divisible(min(args[2], max_channels) * width, 8)
-        elif m in [GAM]:
-            c1, c2 = args[0], args[1] # ch[f], args[0]
-            if c2 != no:  # if not outputss
-                c2 = make_divisible(c2 * gw, 8)
-            args = [c1, c2, *args[2:]]
+#         elif m in [GAM]:
+#             c1, c2 = args[0], args[1] # ch[f], args[0]
+#             if c2 != no:  # if not outputss
+#                 c2 = make_divisible(c2 * gw, 8)
+#             args = [c1, c2, *args[2:]]
         else:
             c2 = ch[f]
 
