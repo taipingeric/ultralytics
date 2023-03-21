f711a31d6b6a33a1f75ab389f5035dcd44804613
SAI <taipingeric@gmail.com>
2023-03-21 15:36:35 +0800

    Update tasks.py
    for CBAM

--- ultralytics/nn/tasks.py
+++ ultralytics/nn/tasks.py
@@ -443,7 +443,14 @@ def parse_model(d, ch, verbose=True):  # model_dict, input_channels(3)
     ch = [ch]
     layers, save, c2 = [], [], ch[-1]  # layers, savelist, ch out
     for i, (f, n, m, args) in enumerate(d['backbone'] + d['head']):  # from, number, module, args
-        m = getattr(torch.nn, m[3:]) if 'nn.' in m else globals()[m]  # get module
+        if 'nn.' in m:
+            m = getattr(torch.nn, m[3:])  
+        elif 'CBAM' in m:
+            m = CBAM
+        else:
+            m = globals()[m]
+#         m = getattr(torch.nn, m[3:]) if 'nn.' in m else globals()[m]  # get module
+        
         for j, a in enumerate(args):
             if isinstance(a, str):
                 with contextlib.suppress(ValueError):
