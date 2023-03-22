0857dd04af0038720693467fcdb12282286eae7b
SAI <taipingeric@gmail.com>
2023-03-22 16:23:33 +0800

    Update tasks.py
    add BAM

--- ultralytics/nn/tasks.py
+++ ultralytics/nn/tasks.py
@@ -10,7 +10,7 @@ import torch.nn as nn
 
 from ultralytics.nn.modules import (C1, C2, C3, C3TR, SPP, SPPF, Bottleneck, BottleneckCSP, C2f, C3Ghost, C3x, Classify,
                                     Concat, Conv, ConvTranspose, Detect, DWConv, DWConvTranspose2d, Ensemble, Focus,
-                                    GhostBottleneck, GhostConv, Segment, CBAM, GAM, CoordAtt, GAMCBAM)
+                                    GhostBottleneck, GhostConv, Segment, CBAM, GAM, CoordAtt, GAMCBAM, BAM)
 from ultralytics.yolo.utils import DEFAULT_CFG_DICT, DEFAULT_CFG_KEYS, LOGGER, colorstr, emojis, yaml_load
 from ultralytics.yolo.utils.checks import check_requirements, check_suffix, check_yaml
 from ultralytics.yolo.utils.torch_utils import (fuse_conv_and_bn, fuse_deconv_and_bn, initialize_weights,
