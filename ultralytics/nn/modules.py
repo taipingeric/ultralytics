265465b889d7427854d01c3a541094380bf4f0d0
SAI <taipingeric@gmail.com>
2023-03-22 16:43:25 +0800

    Update modules.py
    fix BAM spatial att

--- ultralytics/nn/modules.py
+++ ultralytics/nn/modules.py
@@ -702,13 +702,18 @@ class BAMSpatialAttention(nn.Module):
         self.sa.add_module('bn_reduce1',nn.BatchNorm2d(channel//reduction))
         self.sa.add_module('relu_reduce1',nn.ReLU())
         for i in range(num_layers):
-            self.sa.add_module('conv_%d'%i,nn.Conv2d(kernel_size=3,in_channels=channel//reduction,out_channels=channel//reduction,padding=1,dilation=dia_val))
+            self.sa.add_module('conv_%d'%i,nn.Conv2d(kernel_size=3,
+                                                     in_channels=channel//reduction,
+                                                     out_channels=channel//reduction,
+                                                     padding=autopad(3, d=dia_val),
+                                                     dilation=dia_val))
             self.sa.add_module('bn_%d'%i,nn.BatchNorm2d(channel//reduction))
             self.sa.add_module('relu_%d'%i,nn.ReLU())
         self.sa.add_module('last_conv',nn.Conv2d(channel//reduction,1,kernel_size=1))
 
     def forward(self, x) :
         res=self.sa(x)
+        print(x.shape, res.shape)
         res=res.expand_as(x)
         return res
 
